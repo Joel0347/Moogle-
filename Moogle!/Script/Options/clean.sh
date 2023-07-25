7742de351f6f8fc8f972e73a83f5f4a97ab931b6
Joel0347 <joelat0307@gmail.com>
2023-07-24 22:36:57 -0400

    updating clean

--- Moogle!/Script/Options/clean.sh
+++ Moogle!/Script/Options/clean.sh
@@ -7,6 +7,14 @@ clean() {
     rm -f *.aux *.lot *.lof *.log *.toc *.dvi *.ps *.bbl *.out *.synctex.gz *.fls *.fdb_latexmk
 
     cd ..
+
+    # eliminamos carpetas auxiliares de MoogleServer y MoogleEngine.
+    cd MoogleServer
+    rm -r bin obj
+    cd ..
+    cd MoogleEngine
+    rm -r bin obj 
+    cd ..
     # eliminamos los archivos auxiliares de la presentacion
     cd 'Presentación del proyecto'
     rm -f *.aux *.lot *.lof *.log *.toc *.dvi *.ps *.bbl *.out *.synctex.gz *.fls *.fdb_latexmk *.nav *.snm *.vrb
